b7aa7d95b05e203446e89465b00202f5750349bd
Eu, de paula <gfpaula.cic@uesc.br>
2024-11-21 01:12:17 -0300

    furst files implementation

--- lua_array/arrayImp.lua
+++ lua_array/arrayImp.lua
@@ -1,3 +1,18 @@
 --[[
 implementation file of array in lua, basically a class
+    instructions:
+                read a word and put in an array table
+                use n numbers of letter arrangment 
+                where n is the number of letter in the original word
+                n  number of new words 
+                print all
 --]]
+
+local function main()
+    io.write("say: ");
+    local originalWord=io.read("*l");
+    --todo; read the word, alocate into and 1d table using newindex
+    --next: make the operation to create the new words and allocate in the 2d table
+end
+
+main();
