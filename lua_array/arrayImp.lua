59453eb9a8421330e1c9c9e8c53bede5cecec210
Eu, de paula <gfpaula.cic@uesc.br>
2024-11-21 17:39:57 -0300

    improviments in implementation

--- lua_array/arrayImp.lua
+++ lua_array/arrayImp.lua
@@ -7,12 +7,57 @@ implementation file of array in lua, basically a class
                 n  number of new words 
                 print all
 --]]
+local metaT={
+    __newindex=function(table, key, value)
+        
+            rawset(table,key, value)
+    end
+}
+--[[
+newWords
+receives the table with the word tokenized
+rearange into new words with n numbers of the tolkken
+1 = the plain word
+2=the index of indexes 
+3 and beyond= the words
+--]]
+local function newWords(tableWord, size)
+    
+    for i=2, size do    --run the lines
+        for ii=1, size do--run the collumns
+            tableWord[i][ii]= 0;--initialize the array index
+             ::continue::
+            local randomNumber= math.random(size)--get a random number
+                for iii = 1, ii do--to run the line auntil that point
+                   
+                    if randomNumber==tableWord[i][iii] then--if find the number means its already taken and should redo the randon
+                        goto continue;
+                    else
+                        tableWord[i][ii]=randomNumber;
+                        break;
+                    end
+                    
+                end
+        end
+    end
+end
 
 local function main()
     io.write("say: ");
     local originalWord=io.read("*l");
+    local tableWord={};
+    local size=0;
+    setmetatable(tableWord,metaT);
+    for i=1, #originalWord do
+        tableWord[1][i]= " ";
+        tableWord[1][i]=string.sub(originalWord, i, i);
+        size=size+1;
+    end
+    newWords(tableWord);
+
     --todo; read the word, alocate into and 1d table using newindex
     --next: make the operation to create the new words and allocate in the 2d table
+
 end
 
 main();
